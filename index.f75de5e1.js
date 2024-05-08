bd2bb92d193e4ff864ee2fcc0e9322257b47e4bf
Denis Hurskiy <denis.gurskiy@gmail.com>
2024-05-08 11:59:35 +0200

    make build

---
+++ index.f75de5e1.js
@@ -0,0 +1,18 @@
+"use strict";
+document.addEventListener("click", (e)=>{
+    const spider = document.querySelector(".spider");
+    const wall = document.querySelector(".wall");
+    if (e.target.className !== "wall") return;
+    const wallRect = wall.getBoundingClientRect();
+    const wallBorder = parseInt(window.getComputedStyle(wall).borderTopWidth);
+    const wallEdgeBottom = wallRect.bottom - wallRect.top - wallBorder * 2;
+    const wallEdgeRight = wallRect.right - wallRect.left - wallBorder * 2;
+    let height = e.clientY - spider.clientHeight / 2 - wallRect.top - wallBorder;
+    let left = e.clientX - spider.clientWidth / 2 - wallRect.left - wallBorder;
+    height = Math.min(Math.max(0, height), wallEdgeBottom - spider.clientHeight);
+    left = Math.min(Math.max(0, left), wallEdgeRight - spider.clientWidth);
+    spider.style.top = `${height}px`;
+    spider.style.left = `${left}px`;
+});
+
+//# sourceMappingURL=index.f75de5e1.js.map
